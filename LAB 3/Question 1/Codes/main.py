2845e9579ec7f401b97966f6c4d6a16c2de22c5f
arveus <48393979+Akshat-Raj-Vansh@users.noreply.github.com>
2022-01-27 20:11:42 +0530

    added Question 1

---
+++ LAB 3/Question 1/Codes/main.py
@@ -0,0 +1,34 @@
+__author__ = 'Akshat Raj Vansh'
+__version__ = '0.1.0'
+__license__ = 'MIT'
+
+import random
+import string
+import os
+
+
+def generateRandomLines():
+    text = []
+    for i in range(0, 20000):
+        text.append(''.join(random.choices(
+            string.ascii_letters+string.digits, k=20))+'\n')
+    return text
+
+
+def writeToFile(filename):
+    file = open(filename.encode('unicode_escape'), 'w+')
+    randomString = generateRandomLines()
+    file.writelines(randomString)
+    file.close()
+
+
+def main():
+    directory = os.getcwd()
+    directory = directory[:directory.rfind("\\")]
+    filename = directory+'\Files\textfile'
+    for i in range(1, 501):
+        writeToFile(filename+str(i)+'.txt')
+
+
+if __name__ == '__main__':
+    main()
---
+++ LAB 3/Question 1/README.md
@@ -0,0 +1,5 @@
+# LAB 3
+
+## Question 1
+
+    Q. Create 500 txt files in a directory. Every file contains 20,000 lines and every line contain random string of length 20 characters.
